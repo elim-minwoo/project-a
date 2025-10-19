fd15195e765c0825ad13c837b65c39a05a8a0a5e
elim-minwoo <minwoo.han@s.elimmtalbert.school.nz>
2025-10-20 10:41:41 +1300

    smooth camera

--- scenes/menus/StartMenui.gd
+++ scenes/menus/StartMenui.gd
@@ -11,4 +11,4 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/levels/Test.tscn")
+	get_tree().change_scene_to_file("res://scenes/levels/LevelDev.tscn")
