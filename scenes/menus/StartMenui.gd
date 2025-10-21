60da6663217c42010d480ad0802aa7b544bd2a7c
elim-minwoo <minwoo.han@s.elimmtalbert.school.nz>
2025-10-21 14:57:18 +1300

    parallax bg apply

--- scenes/menus/StartMenui.gd
+++ scenes/menus/StartMenui.gd
@@ -1,7 +1,11 @@
 extends Control
 const LEVEL_DEV = preload("uid://r3t8vyh6hk05")
+const PARALLAX_PLACEHOLDER = preload("uid://bakqi37iis4tj")
 
 func _on_start_button_pressed() -> void:
 	Global.game_layer.load_scene(LEVEL_DEV)
 	Global.ui_layer.unload_current()
+	
+	var _inst = PARALLAX_PLACEHOLDER.instantiate()
+	Global.game_node.add_child(_inst)
 	
