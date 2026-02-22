231079ac20a72d92610f46b82db764c75fdba72b
elim-minwoo <minwooh0721@gmail.com>
2026-02-23 07:23:03 +1300

    hover outline test

--- scenes/enemies/dummy/dummy.gd
+++ scenes/enemies/dummy/dummy.gd
@@ -1,8 +1,14 @@
-extends CharacterBody2D
+extends Area2D
 
 @onready var dummy_sprite: Sprite2D = $Sprite2D
 
 const DUMMY_PROJECTILE = preload("uid://coednfjc0rx2q")
+
+
+
+func _ready() -> void:
+	mouse_entered.connect(_on_hovered.bind(true))
+	mouse_exited.connect(_on_hovered.bind(false))
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("debug_input"):
@@ -10,9 +16,6 @@ func _physics_process(delta: float) -> void:
 		projectile_temp.direction = -1
 		add_child(projectile_temp)
 
-
-func _on_mouse_entered() -> void:
-	pass
-
-func _on_mouse_exited() -> void:
-	pass
+func _on_hovered(hovered: bool) -> void:
+	print(dummy_sprite.material)
+	dummy_sprite.material.set_shader_parameter("thickness", 0.8 if hovered else 0.0)
